d48fc7f546d568b5e70f9daf4d7f27e8b5f81490
nguyenhoangnhan2003 <140692347+nguyenhoangnhan2003@users.noreply.github.com>
2024-01-26 16:31:17 +0700

    26/01/2023

--- reactbasic/src/views/App.js
+++ reactbasic/src/views/App.js
@@ -10,14 +10,6 @@ function App() {
         <p>
           Hello World with React.js (Hoi Dan IT)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <MyComponent />
       </header>
     </div>
--- reactbasic/src/views/Example/MyComponent.js
+++ reactbasic/src/views/Example/MyComponent.js
@@ -1,9 +1,50 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        firstName: '',
+        lastName: '',
+    }
+
+    handleOnChangeFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    handleOnChangeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleSummit = (event) => {
+        event.preventDefault()
+        alert('Click me!!!')
+    }
+
     render() {
+        console.log('>>>Call render:', this.state);
         return (
-            <div>Hello my component Hoi Dan IT</div>
+            <>
+                <form >
+                    <label htmlFor="fname">First name:</label><br />
+                    <input
+                        type="text"
+                        value={this.state.firstName}
+                        onChange={(event) => this.handleOnChangeFirstName(event)}
+                    />
+                    <br />
+                    <label htmlFor="lname">Last name:</label><br />
+                    <input type="text" value={this.state.lastName}
+                        onChange={(event) => this.handleOnChangeLastName(event)}
+                    /><br /><br />
+                    <button type="submit"
+                        onClick={(event) => this.handleSummit(event)}
+                    />
+                </form>
+            </>
         )
     }
 }
